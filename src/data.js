34891bb73ec62f0eae88ad2d8b47e9f334fb0193
Furkan Özdemir <furkanozd1231@gmail.com>
2023-06-10 22:42:42 +0300

    kelime oyunu
    kelime oyunu

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const DATA =[
+    {question:'Bir Çok Bilgisayar Sisteminin Birbirine Bağlı Olduğu, Dünya Çapında Yaygın Olan Ve Sürekli Büyüyen İletişim Ağına Ne Denir?',answer:'internet'},
+    {question:'Bir Tablo Veya Resmin Genişliğini Ayarlamak İçin Kullanılan HTML Parametresi Aşağıdakilerden Hangisidir?',answer:'width'},
+    {question:'Bir Yazının Yazı Karakter Boyutunu Değiştirmek İçin Kullanılan HTML Parametresi Aşağıdakilerden Hangisidir?',answer:'size'},
+    {question:'Bilgisayarda Yazmış Olduğumuz Herhangi Bir Belgenin Kağıda Aktarılmasını Sağlayan Aygıta Ne Ad Verilir?',answer:'printer'}
+    
+    
+]
